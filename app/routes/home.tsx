ef50a060382dfb169e8fa1a33328f93b1f366927
Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
2025-09-17 11:02:59 -0500

    feat: set up HeroUI integration and optimize Dockerfile

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,5 @@
+import { Button, ButtonGroup, Checkbox } from '@heroui/react';
+
 export function meta() {
 	return [
 		{ title: 'SIRHA - DOSW' },
@@ -9,6 +11,9 @@ export default function Home() {
 	return (
 		<div className="w-dvw h-dvh grid place-items-center">
 			<h1 className="text-7xl">Hola DOSW</h1>
+			<Button color="primary" size="lg" className="mt-4">
+				¡Bienvenido a SIRHA - DOSW!
+			</Button>
 		</div>
 	);
 }
